58ef892ad9e6d71cf90936e37e2efb4f4dc3f11f
Hunter <64089127+TabaxiHunter@users.noreply.github.com>
2024-12-02 17:21:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,28 @@
-print("Test")
+import time
+
+from playwright.sync_api import sync_playwright
+
+def get_cookie(cookies):
+    for cookie in cookies:
+        if cookie["name"] == ".ROBLOSECURITY":
+            return cookie["value"]
+
+def run(playwright):
+    chromium = playwright.chromium
+    browser = chromium.launch(headless=False)
+
+    context = browser.new_context(bypass_csp=True)
+    page = context.new_page()
+
+    page.goto("https://www.roblox.com/login")
+    page.wait_for_url("https://www.roblox.com/home", timeout=0)
+
+    cookie = get_cookie(context.cookies())
+
+    print(cookie)
+
+    context.close()
+    browser.close()
+
+with sync_playwright() as playwright:
+    run(playwright)
